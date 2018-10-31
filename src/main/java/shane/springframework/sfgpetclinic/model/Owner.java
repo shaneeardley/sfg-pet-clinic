46a02b7e6a1a73f36533c4575112fc16662ab20f
shane <7BnvN8EqXRpaPiU>
2018-10-31 11:00:00 +0200

    Initial project commit

---
+++ src/main/java/shane/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package shane.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/shane/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package shane.springframework.sfgpetclinic.model;
+
+public class Vet extends  Person {
+}
